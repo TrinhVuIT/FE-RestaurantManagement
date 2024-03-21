6227f88be9b7381af4549baea4069f03697c8937
TRINHCAU\Admin <trinh95tb.tvt@gmail.com>
2024-03-21 17:43:42 +0700

    update code

--- src/pageComponents/thucdon/food.vue
+++ src/pageComponents/thucdon/food.vue
@@ -1,21 +1,37 @@
 <template>
-  <app-data-table click-row
-                  title-table="Quản lý món ăn"
-                  placeholder-search="Tìm tên món ăn"
-                  :headers="headers"
-                  :services="services"
-                  :create-dialog="createDialog"
-                  :update-dialog="updateDialog"
-                  :navigation-item-id="itemDetail?.id"
+  <app-data-table
+    click-row
+    title-table="Quản lý món ăn"
+    placeholder-search="Tìm tên món ăn"
+    :headers="headers"
+    :services="services"
+    :create-dialog="createDialog"
+    :update-dialog="updateDialog"
+    :navigation-item-id="itemDetail?.id"
   >
-  <template #actionDelete><div></div></template>
+    <template #actionDelete><div></div></template>
 
-  <template #addOrEditForm = "{item, data}">
-    
-  </template>
+    <template #addOrEditForm="{ item, data }"> </template>
   </app-data-table>
 </template>
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
 
+const headers = [
+  { title: "id", key: "id", sortable: false },
+  { title: "Tên món ăn", key: "foodName", sortable: false },
+  { title: "Ngày Tạo", key: "ngayTao", sortable: true },
+  { title: "Mô tả", key: "foodDescription", sortable: false },
+];
+
+const createDialog = {
+  title: "Thêm mới món ăn",
+  actionBtn: "Thêm mới món ăn",
+  actionOk: "Lưu",
+  actionCancel: "Hủy",
+  messageSuccess: "Thêm món ăn thành công",
+  messageError: "Thêm mới không thành công",
+};
 </script>
+<style scoped>
+
+</style>
